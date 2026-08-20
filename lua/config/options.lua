e9844e238b3133eca9a5e3121e54b735b7d4b718
Daniel Strong <daniel.strong@ncino.com>
2026-08-20 15:25:45 -0400

    add comment

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -300,6 +300,7 @@ end)
 -- opt.statusline =
 -- "%<%f %h%w%m%r %{% v:lua.require('vim._core.util').term_exitcode() %}%=%{% luaeval('(package.loaded[''vim.ui''] and vim.api.nvim_get_current_win() == tonumber(vim.g.actual_curwin or -1) and vim.ui.progress_status()) or '''' ')%}%{% &showcmdloc == 'statusline' ? '%-10.S ' : '' %}%{% exists('b:keymap_name') ? '<'..b:keymap_name..'> ' : '' %}%{% &busy > 0 ? '◐ ' : '' %}%{% luaeval('(package.loaded[''vim.diagnostic''] and next(vim.diagnostic.count()) a nd vim.diagnostic.status() .. '' '') or '''' ') %}%{% &ruler ? ( &rulerformat == '' ? '%-14.(%l,%c%V%) %P' : &rulerf ormat ) : '' %}"
 
+-- This isolates marks, history, recent files, etc per project instead of sharing them globally
 -- vim.opt.exrc = true
 -- vim.opt.secure = true
 local workspace_path = vim.fn.getcwd()
