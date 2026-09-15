2f9f28b3d60bad66ce25572ae8b7b66210cde2a1
Daniel Strong <daniel.strong@ncino.com>
2026-09-15 17:30:34 -0400

    add clue

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -965,9 +965,113 @@ return {
         end,
     },
     {
+        "nvim-mini/mini.clue",
+        version = false,
+        enabled = true,
+        config = function(_, opts)
+            local miniclue = require("mini.clue")
+            miniclue.setup({
+
+                -- Array of opt-in triggers which start custom key query process.
+                -- **Needs to have something in order to show clues**.
+                triggers = {
+                    -- Leader triggers
+                    { mode = { "n", "x" }, keys = "<Leader>" },
+                    { mode = { "n", "x" }, keys = "<LocalLeader>" },
+
+                    -- `[` and `]` keys
+                    { mode = "n", keys = "[" },
+                    { mode = "n", keys = "]" },
+
+                    -- Built-in completion
+                    { mode = "i", keys = "<C-x>" },
+
+                    -- `g` key
+                    { mode = { "n", "x" }, keys = "g" },
+
+                    -- Marks
+                    { mode = { "n", "x" }, keys = "'" },
+                    { mode = { "n", "x" }, keys = "`" },
+
+                    -- Registers
+                    { mode = { "n", "x" }, keys = '"' },
+                    { mode = { "i", "c" }, keys = "<C-r>" },
+
+                    -- Window commands
+                    { mode = "n", keys = "<C-w>" },
+
+                    -- `z` key
+                    { mode = { "n", "x" }, keys = "z" },
+                },
+
+                -- Array of extra clues to show
+                clues = {
+                    -- { mode = "n", keys = "]b", postkeys = "]" },
+                    -- { mode = "n", keys = "]w", postkeys = "]" },
+
+                    -- { mode = "n", keys = "[b", postkeys = "[" },
+                    -- { mode = "n", keys = "[w", postkeys = "[" },
+                    { mode = "n", keys = "<localleader>b", desc = "+Buffers" },
+                    { keys = "<localleader>a", desc = "+Actions", mode = { "n", "x" } },
+                    { keys = "<localleader>o", desc = "+Diffview", mode = { "n", "x" } },
+                    { keys = "<localleader>K", desc = "+LSP Buffer", mode = { "n", "x" } },
+                    { keys = "<localleader>N", desc = "+Nvim Raw", mode = { "n", "x" } },
+                    { keys = "<localleader>Q", desc = "+Quick", mode = { "n", "x" } },
+                    { keys = "<localleader>W", desc = "+Save", mode = { "n", "x" } },
+                    { keys = "<localleader>d", desc = "+Diff Tools", mode = { "n", "x" } },
+                    { keys = "<localleader>f", desc = "+Fuzzy", mode = { "n", "x" } },
+                    { keys = "<localleader>g", desc = "+Git", mode = { "n", "x" } },
+                    { keys = "<localleader>h", desc = "+Hunk", mode = { "n", "x" } },
+                    { keys = "<localleader>j", desc = "+Copy Store", mode = { "n", "x" } },
+                    { keys = "<localleader>k", desc = "+Language Tools", mode = { "n", "x" } },
+                    { keys = "<localleader>n", desc = "+Nvim", mode = { "n", "x" } },
+                    { keys = "<localleader><localleader>", desc = "+Ctrl", mode = { "n", "x" } },
+                    { keys = "<C-q>", desc = "+Tabs", mode = { "n", "x" } },
+                    { keys = "<C-q>m", desc = "+Move Tab", mode = { "n", "x" } },
+                    { keys = "<localleader>t", desc = "+Tabs", mode = { "n", "x" } },
+                    { keys = "<localleader>tm", desc = "+Move Tab", mode = { "n", "x" } },
+                    { keys = "<localleader>r", desc = "+Replace", mode = { "n", "x" } },
+                    { keys = "<localleader>u", desc = "+UI", mode = { "n", "x" } },
+                    { keys = "<localleader>z", desc = "+Session", mode = { "n", "x" } },
+                    { keys = "Z", desc = "+File", mode = { "n", "x" } },
+                    { keys = "gm", desc = "+Modify Code", mode = { "n", "x" } },
+                    { keys = "<localleader>=", desc = "+Fix Indention", mode = { "n", "x" } },
+                    { keys = "<localleader>=z", desc = "+Formatters", mode = { "n", "x" } },
+                    miniclue.gen_clues.square_brackets(),
+                    miniclue.gen_clues.builtin_completion(),
+                    miniclue.gen_clues.g(),
+                    miniclue.gen_clues.marks(),
+                    miniclue.gen_clues.registers(),
+                    miniclue.gen_clues.windows(),
+                    miniclue.gen_clues.z(),
+                },
+                --
+                -- Clue window settings
+                window = {
+                    -- Floating window config
+                    config = {
+
+                        -- Compute window width automatically
+                        width = "auto",
+
+                        -- Use double-line border
+                        border = "rounded",
+                    },
+
+                    -- Delay before showing clue window
+                    delay = 200,
+
+                    -- Keys to scroll inside the clue window
+                    scroll_down = "<C-d>",
+                    scroll_up = "<C-u>",
+                },
+            })
+        end,
+    },
+    {
         "folke/which-key.nvim",
         lazy = true,
-        -- enabled = false,
+        enabled = false,
         opts = {
             delay = function(ctx)
                 -- vim.notify(vim.inspect(ctx))
