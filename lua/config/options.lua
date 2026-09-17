c82b895940d6ab1a271cbd4d326c5ca24f7e6e69
Daniel Strong <daniel.strong@ncino.com>
2026-09-17 16:34:24 -0400

    newer

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -294,7 +294,8 @@ vim.opt.foldtext = "v:lua.custom_fold_text()"
 
 -- neovim's actual built-in default statusline (see `nvim --clean` vim.o.statusline) with filetype appended
 local nvim_default_statusline = table.concat({
-    [[%<%f %a%h%w%m%r ]],
+    -- [[%<%f %a%h%w%m%r ]],
+    [[%<%f %{% v:lua.require('custom-utils.buffer_numbers').ruler_slot() %}%a%h%w%m%r ]],
     [[%{% v:lua.require('vim._core.util').term_exitcode() %}]],
     [[%=]],
     [[%{% luaeval('(package.loaded[''vim.ui''] and vim.api.nvim_get_current_win() == tonumber(vim.g.actual_curwin or -1) and vim.ui.progress_status()) or '''' ')%}]],
